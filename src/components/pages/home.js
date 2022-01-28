c49fc2699d458621579bc9fbaa4b7fcc6d9bed2c
sanjee gwachha <gwachhas123@gmail.com>
2022-01-28 12:54:19 +0545

    fourth commit

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import '../../App.css';
+import herosection from '../herosection';
+
+function home() {
+    return(
+        <>
+        <herosection />
+        </>
+    );
+}
+
+export default home;
